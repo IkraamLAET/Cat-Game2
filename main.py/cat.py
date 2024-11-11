e0cf9a366d12c806bc3e16b2410228f84b48374d
IkraamLAET <Ikraam.Abukar@laetottenham.ac.uk>
2024-11-11 12:05:58 +0000

    hi

--- main.py/cat.py
+++ main.py/cat.py
@@ -11,28 +11,36 @@ class Cat:
         self.weight = 5
     
     def train(self):
-        print(f"{self.name} is training ...")
+        print(f"{self.name} is training ...  -5 energy  +1 intelligence")
         self.energy -= 5
         self.intelligence += 1
         self.age += 0.1
 
     def feed(self):
-        print(f"{self.name} is eating ...")
-        self.energy += 1
+        print(f"{self.name} is eating ...  +10 energy  +1 weight")
+        self.energy += 10
         self.weight += 1
         self.age += 0.1
 
     def sleep(self):
-        print(f"{self.name} is training ...")
+        print(f"{self.name} is sleeping ...  +10 energy  ")
         self.energy += 10
         self.age += 0.1
 
     def play(self):
-        print(f"{self.name} is playing ...")
+        print(f"{self.name} is playing ...  -5 energy  -1 weight")
         self.energy -= 5
         self.weight -= 1
         self.age -= 0.1
 
     def stats(self):
         print(f"\n {self.name}'s stats: \n Energy = {self.energy} \n Intelligence = {self.intelligence} \n Weight = {self.weight}")
+    
+    def too_high(self):
+            print(f"\n{self.name}'s energy and weight are too high! try training/playing with it.")
+    
+    def old_cat(self):
+        if self.age>=15:
+            print(f"\n{self.name} became too old and died. Game over.")
+
 
--- main.py/main.py
+++ main.py/main.py
@@ -1,7 +1,36 @@
 from cat import Cat
+import pyfiglet as pyg
 
-print("Welcome to the Cat Game!")
-
+title=pyg.figlet_format("Welcome to the Cat Game!")  
+print(title)
+picture = """                                  .--.
+                                               `.  \
+                                                 \  \
+                                                  .  \
+                                                  :   .
+                                                  |    .
+                                                  |    :
+                                                  |    |
+  ..._  ___                                       |    |
+ `."".`''''""--..___                              |    |
+ ,-\  \             ""-...__         _____________/    |
+ / ` " '                    `""""""""                  .
+ \                                                      L
+ (>                                                      \
+/                                                         \
+\_    ___..---.                                            L
+  `--'         '.                                           \
+                 .                                           \_
+                _/`.                                           `.._
+             .'     -.                                             `.
+            /     __.-Y     /''''''-...___,...--------.._            |
+           /   _."    |    /                ' .      \   '---..._    |
+          /   /      /    /                _,. '    ,/           |   |
+          \_,'     _.'   /              /''     _,-'            _|   |
+                  '     /               `-----''               /     |
+                  `...-'                                       `...-'
+                  """
+print(picture)
 name=input("Enter cat name: ")
 colour = input("Enter cat colour: ")
 
@@ -25,3 +54,8 @@ What would you like to do?
         my_cat.sleep()
     elif action == 5:
         my_cat.stats()
+
+    if my_cat.energy>=75 or my_cat.weight>=10:
+        my_cat.too_high()
+    if my_cat.age>=15:
+        my_cat.too_old()
